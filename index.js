5f7a5773c079d088d8573cec9f431db7bd5a5560
Thiago Moraes <thiago4x70@gmail.com>
2021-08-12 20:38:14 -0300

    add tests for createFile method

--- index.js
+++ index.js
@@ -11,33 +11,33 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/public", express.static(__dirname + "/public/"));
 
 app.get("/", (req, res) => {
-  fileManagement.getAllFiles((err, data) =>
-    res.render("pages/index", {
-      articles: data,
-      content: null,
-      selectedFile: null
-    })
-  );
+    fileManagement.getAllFiles((err, data) =>
+        res.render("pages/index", {
+            articles: data,
+            content: null,
+            selectedFile: null
+        })
+    );
 });
 
 app.get("/data/:file", (req, res) => {
-  const files = readdirSync("./data");
-  const data = fileManagement.getFile(req.params.file);
-  res.render("pages/index", {
-    articles: files,
-    content: data,
-    selectedFile: req.params.file
-  });
+    const files = readdirSync("./data");
+    const data = fileManagement.getFile(req.params.file);
+    res.render("pages/index", {
+        articles: files,
+        content: data,
+        selectedFile: req.params.file
+    });
 });
 
 app.post("/new-file", (req, res) => {
-  fileManagement.createFileSafe(req.body.filename);
-  res.redirect(`/data/${req.body.filename}`);
+    fileManagement.createFileSafe(req.body.filename);
+    res.redirect(`/data/${req.body.filename}`);
 });
 
 app.post("/save-file", ({ body }, res) => {
-  fileManagement.saveFile(filename, contents);
-  res.redirect(`/data/${filename}`);
+    fileManagement.saveFile(filename, contents);
+    res.redirect(`/data/${filename}`);
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
