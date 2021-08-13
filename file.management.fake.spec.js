40666102d6dbd0e29da089547d9a12519352148d
Thiago Moraes <thiago4x70@gmail.com>
2021-08-13 00:39:03 -0300

    add fakes test file

---
+++ file.management.fake.spec.js
@@ -0,0 +1,44 @@
+const { expect } = require("chai");
+const sinon = require("sinon");
+const fs = require("fs");
+const proxyquire = require("proxyquire");
+const fileManagement = require("./file.management");
+
+
+describe('File Management Fake', () => {
+    afterEach(() => {
+        sinon.restore();
+    });
+
+    it.skip('Should create a new file', () => {
+        const writeFake = sinon.fake(fs.writeFileSync);
+        sinon.replace(fs, 'writeFileSync', writeFake);
+
+        const fileManagement = proxyquire('./file.management', { fs });
+        fileManagement.createFile('test.txt');
+
+        expect(writeFake.calledWith('./data/test.txt')).to.be.true;
+    });
+
+    it('Should throw an exception when the file already exists', () => {
+        const writeFake = sinon.fake.throws(new Error());
+        sinon.replace(fs, 'writeFileSync', writeFake);
+
+        const fileManagement = proxyquire('./file.management', { fs });
+        const createFile = () => fileManagement.createFile('test.txt');
+        
+        expect(createFile).to.throw();
+    });
+
+    it('#getAllFiles() should return a list of files', () => {
+        const readFake = sinon.fake.yields(null, ['test.txt']);
+        
+        sinon.replace(fs, 'readdir', readFake);
+
+        const fileManagement = proxyquire('./file.management', { fs });
+
+        fileManagement.getAllFiles((err, data) => {
+            expect(data).to.eql(['test.txt']);
+        });
+    });
+});
